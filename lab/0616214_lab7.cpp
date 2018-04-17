640410ec7851c6456c4c433fb62699e9aeb1a98e
finch185277 <59487andy@gmail.com>
2018-04-17 19:29:12 +0800

    Update 0616214_lab7.cpp

--- lab/0616214_lab7.cpp
+++ lab/0616214_lab7.cpp
@@ -10,7 +10,6 @@ int main() {
                 int upCount = 0;
                 int downCount = 0;
                 std::vector<int> wall;
-                std::vector<int>::iterator it;
 
                 std::cin >> itemCount;
                 for(int i = 0; i < itemCount; i++) {
@@ -18,7 +17,9 @@ int main() {
                         std::cin >> temp;
                         wall.push_back(temp);
                 }
-                for(it = wall.begin(); it != wall.end() - 1; it++) {
+                for(auto it = wall.begin(); it != wall.end() - 1; it++) {
+                        // using g++ -std=c++11 to compile "auto"
+                        // but oj would break XD
                         if(*it > *(it+1)) {
                                 downCount++;
                                 continue;
@@ -33,4 +34,3 @@ int main() {
         }
         return 0;
 }
-
