19a06d414205b775bba4c9f70499e518ffd8e515
finch185277 <59487andy@gmail.com>
2018-04-30 00:23:00 +0800

    Update main.cpp

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -4,23 +4,25 @@
 
 using namespace std;
 
+using type = float;
+
 int main()
 {
 
         // test default constructor
-        Link_List<int> linkList1;
+        Link_List<type> linkList1;
         cout << "linkList1 Size: " << linkList1.getSize() << endl; ////////////
         // cout << "linkList1 insert: " << linkList1.insert_node(2, 3) << endl; ////////////
 
         // test copy constructor
-        Link_List<int> linkList2(linkList1);
+        Link_List<type> linkList2(linkList1);
 
         // test getSize()
         cout << "linkList2 Size: " << linkList2.getSize() << endl;
 
         // test insert_node(value), delete_node(), operator<<, operator>>
 
-        Link_List<int> linkList3;
+        Link_List<type> linkList3;
         cout << "Enter a integer: ";
         cin >> linkList3;
         cout << "linkList3: "<< linkList3 << endl;
@@ -29,7 +31,7 @@ int main()
         linkList3.insert_node(12);
         linkList3.insert_node(13);
         linkList3.insert_node(14);
-        linkList3.insert_node(15);
+        linkList3.insert_node(3,17);
         cout << "Insert Boolean: " << linkList3.insert_node(16) << endl;
         cout << "linkList3: " << linkList3 << endl;
 
@@ -37,7 +39,7 @@ int main()
         cout << "linkList3: " << linkList3 << endl;
 
         // test assignment operator, equality operator, insert_node(index, value), delete_node(index)
-        Link_List<int> linkList4 = linkList3;
+        Link_List<type> linkList4 = linkList3;
         cout << "linkList4: " << linkList4 << endl;
 
         cout << "Insert Boolean: " << linkList4.insert_node(3, 17) << endl;
@@ -48,7 +50,7 @@ int main()
         cout << "linkList4: " << linkList4 << endl;
 
         // test subscript operator []
-        const Link_List<int> linkList5 = linkList4;
+        const Link_List<type> linkList5 = linkList4;
         cout << "linkList4[1]: " << linkList4[1] << endl;
         cout << "linkList5[1]: " << linkList5[1] << endl;
 
