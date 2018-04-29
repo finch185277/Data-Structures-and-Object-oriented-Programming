fe9574b301ebf3961648908cb882cecda25b85bc
finch185277 <59487andy@gmail.com>
2018-04-30 00:37:19 +0800

    Update main.cpp

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-#include "0616214_Link_List.h"
+#include "0616214_Link_List.hpp"
 
-using namespace std;
+// using namespace std; // no need
 
 using type = float;
 
@@ -11,48 +11,46 @@ int main()
 
         // test default constructor
         Link_List<type> linkList1;
-        cout << "linkList1 Size: " << linkList1.getSize() << endl; ////////////
-        // cout << "linkList1 insert: " << linkList1.insert_node(2, 3) << endl; ////////////
 
         // test copy constructor
         Link_List<type> linkList2(linkList1);
 
         // test getSize()
-        cout << "linkList2 Size: " << linkList2.getSize() << endl;
+        std::cout<< "linkList2 Size: " << linkList2.getSize() << std::endl;
 
         // test insert_node(value), delete_node(), operator<<, operator>>
 
         Link_List<type> linkList3;
-        cout << "Enter a integer: ";
-        cin >> linkList3;
-        cout << "linkList3: "<< linkList3 << endl;
+        std::cout<< "Enter a integer: ";
+        std::cin >> linkList3;
+        std::cout << "linkList3: "<< linkList3 << std::endl;
 
         linkList3.insert_node(11);
         linkList3.insert_node(12);
         linkList3.insert_node(13);
         linkList3.insert_node(14);
         linkList3.insert_node(3,17);
-        cout << "Insert Boolean: " << linkList3.insert_node(16) << endl;
-        cout << "linkList3: " << linkList3 << endl;
+        std::cout<< "Insert Boolean: " << linkList3.insert_node(16) << std::endl;
+        std::cout<< "linkList3: " << linkList3 << std::endl;
 
-        cout << "Delete Boolean: " << linkList3.delete_node() << endl;
-        cout << "linkList3: " << linkList3 << endl;
+        std::cout<< "Delete Boolean: " << linkList3.delete_node() << std::endl;
+        std::cout<< "linkList3: " << linkList3 << std::endl;
 
         // test assignment operator, equality operator, insert_node(index, value), delete_node(index)
         Link_List<type> linkList4 = linkList3;
-        cout << "linkList4: " << linkList4 << endl;
+        std::cout<< "linkList4: " << linkList4 << std::endl;
 
-        cout << "Insert Boolean: " << linkList4.insert_node(3, 17) << endl;
-        cout << "linkList4: " << linkList4 << endl;
+        std::cout<< "Insert Boolean: " << linkList4.insert_node(3, 17) << std::endl;
+        std::cout<< "linkList4: " << linkList4 << std::endl;
 
-        cout << "Delete Boolean: " << linkList4.delete_node(4) << endl;
-        cout << "Equality Boolean: " << (linkList4==linkList3) << endl;
-        cout << "linkList4: " << linkList4 << endl;
+        std::cout<< "Delete Boolean: " << linkList4.delete_node(4) << std::endl;
+        std::cout<< "Equality Boolean: " << (linkList4==linkList3) << std::endl;
+        std::cout<< "linkList4: " << linkList4 << std::endl;
 
         // test subscript operator []
         const Link_List<type> linkList5 = linkList4;
-        cout << "linkList4[1]: " << linkList4[1] << endl;
-        cout << "linkList5[1]: " << linkList5[1] << endl;
+        std::cout<< "linkList4[1]: " << linkList4[1] << std::endl;
+        std::cout<< "linkList5[1]: " << linkList5[1] << std::endl;
 
         return 0;
 }
