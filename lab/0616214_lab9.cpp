8816b4d15fa512d7b5568f3b9dcd9e96a9f1f064
finch185277 <59487andy@gmail.com>
2018-05-27 16:02:27 +0800

    Update 0616214_lab9.cpp

--- lab/0616214_lab9.cpp
+++ lab/0616214_lab9.cpp
@@ -1,39 +1,47 @@
-// ref: http://programming-study-notes.blogspot.tw/2014/04/uva-10313-pay-price.html
-// ref: https://github.com/tommy5198/acm/blob/master/uva/uva10313.cpp
-// ref: https://github.com/waiwai444/oj/blob/master/uva/10313%20-%20Pay%20the%20Price/10313.cpp
+// ref: https://github.com/selvpole/OOP_HW_practice/blob/master/Lab/Lab09_Pay_the_Price/0616221_lab9.cpp
 #include <iostream>
 #include <sstream>
+#include <string>
 
-long long dp[301][301] = { 0 };
+const int max_price = 300;
+long long ways[max_price + 1][max_price + 1];
 
+void make_table()
+{
+    // make the table
+    for (int i = 0; i <= max_price; i++)
+        ways[i][0] = 0;
+    for (int j = 0; j <= max_price; j++)
+        ways[0][j] = 1;
+    for (int j = 1; j <= max_price; j++) {
+        for (int i = j; i <= max_price; i++) {
+            if (i - j < j)
+                ways[i][j] = ways[i - j][i - j] + ways[i][j - 1];
+            else
+                ways[i][j] = ways[i - j][j] + ways[i][j - 1];
+        }
+    }
+}
 int main()
 {
-    dp[0][0] = 1;
-    for (int i = 0; i < 301; i++)
-        for (int j = 0; j + i < 301; j++)
-            for (int k = 1; k < 301; k++)
-                dp[j + i][k] += dp[j][k - 1];
-
-    std::string line;
-    int n[3];
-    while (std::getline(std::cin, line)) {
-        std::stringstream ss(line);
-        int i = 0;
-        while (ss >> n[i])
-            i++;
-        switch (i) {
-        case 1:
-            std::cout << dp[n[0]][n[0]] << std::endl;
-            break;
-        case 2:
-            std::cout << dp[n[0]][n[1]] << std::endl;
-            break;
-        case 3:
-            if (n[1] == 0)
-                std::cout << dp[n[0]][n[2]] << std::endl;
-            else
-                std::cout << dp[n[0]][n[2]] - dp[n[0]][n[1] - 1] << std::endl;
-            break;
-        }
+    make_table();
+    std::string s;
+    while (getline(std::cin, s)) {
+        std::stringstream ss(s);
+        int n, l1 = -1, l2 = -1;
+        ss >> n;
+        ss >> l1;
+        ss >> l2;
+        l2 > n ? l2 = n : l2 = l2;
+        l1 > n&& l2 >= 0 ? l1 = n + 1 : l1 = l1;
+        l1 > n&& l2 < 0 ? l1 = n : l1 = l1;
+        if (l2 >= 0 && l1 == 0) // in case (l1 - 1) < 0
+            std::cout << ways[n][l2] << '\n';
+        else if (l2 >= 0)
+            std::cout << ways[n][l2] - ways[n][l1 - 1] << '\n';
+        else if (l1 >= 0)
+            std::cout << ways[n][l1] << '\n';
+        else
+            std::cout << ways[n][n] << '\n';
     }
 }
