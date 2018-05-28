71c758a651fc5b81a130cff2fa5c604146e80f12
finch185277 <59487andy@gmail.com>
2018-05-28 22:48:25 +0800

    Update 0616214_lab2.cpp

--- lab/0616214_lab2.cpp
+++ lab/0616214_lab2.cpp
@@ -1,31 +1,27 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <string>
 
+static bool is_square[100001];
+void build()
+{
+    memset(is_square, false, sizeof is_square);
+    for (int i = 0; i * i < 100001; i++) {
+        is_square[i * i] = true;
+    }
+}
 int main()
 {
-        int a, b, count;
-        int array[100001] = {0};
-
-        for(int i = 1; i*i < 100001; i++)
-        {
-                array[i*i] = 1;
+    build();
+    int a, b;
+    while (std::cin >> a >> b && a > 0 && b > 0) {
+        int count = 0;
+        for (int i = a; i <= b; i++) {
+            if (is_square[i])
+                count++;
         }
-
-        while(true)
-        {
-                cin >> a >> b;
-                count = 0;
-
-                if(a == 0 && b == 0)
-                        break;
-
-                for(int i = a; i <= b; i++)
-                {
-                        if(array[i] == 1)
-                                count++;
-                }
-
-                cout << count << "\n";
-        }
-        return 0;
+        std::cout << count << '\n';
+    }
+    return 0;
 }
