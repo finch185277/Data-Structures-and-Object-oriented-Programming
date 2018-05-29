c6be7675babf86131fb941db7ccf378dd88ea337
finch185277 <59487andy@gmail.com>
2018-05-29 16:55:42 +0800

    Update 0616214_lab8.cpp

--- lab/0616214_lab8.cpp
+++ lab/0616214_lab8.cpp
@@ -1,64 +1,51 @@
-#include <array>
+#include <algorithm>
+#include <cstring>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
 int main()
 {
-    int case_count;
+    int test_count;
     std::string skip;
-    std::cin >> case_count;
-    std::getline(std::cin, skip); // to skip first line
-    while (case_count--) {
+    std::cin >> test_count;
+    std::getline(std::cin, skip);
+    while (test_count--) {
+        int opc = 0;
         std::string s;
         std::getline(std::cin, s);
-        int len = s.length(), opcnt = 0; // operator count
-        std::array<long long, 13> num{ 0 }; // long long num[13] = { 0 };
-        std::array<char, 13> opera{ '0' }; // char opera[13] = { 0 };
-        std::vector<long long> v1;
-        std::vector<long long> v2;
-
-        for (int i = 0; i < len; i++) {
+        char opera[13] = { ' ' };
+        int num[13] = { 0 };
+        for (int i = 0; i < s.size(); i++) {
             if (s[i] == '*' || s[i] == '+')
-                opera[opcnt++] = s[i];
+                opera[opc++] = s[i];
             else
-                num[opcnt] = num[opcnt] * 10 + s[i] - '0';
+                num[opc] = num[opc] * 10 + s[i] - '0';
         }
-        long long maximum = 1, minimum = 0, tmp = 0;
-
-        // maximum
-        for (int i = 0; i <= opcnt; i++) {
-            if (i == 0)
-                tmp = num[0];
+        long long max = 1;
+        long long min = 0;
+        long long tmp = 0;
+        //max + -> *
+        tmp = num[0];
+        for (int i = 0; i <= opc; i++) {
             if (opera[i] == '+') {
                 tmp += num[i + 1];
             } else {
-                v1.push_back(tmp);
+                max *= tmp;
                 tmp = num[i + 1];
             }
         }
-
-        //minimum
-        for (int i = 0; i <= opcnt; i++) {
-            if (i == 0)
-                tmp = num[0];
+        //min * -> +
+        tmp = num[0];
+        for (int i = 0; i <= opc; i++) {
             if (opera[i] == '*') {
                 tmp *= num[i + 1];
             } else {
-                v2.push_back(tmp);
+                min += tmp;
                 tmp = num[i + 1];
             }
         }
-
-        for (std::vector<long long>::iterator i = v1.begin(); i < v1.end(); i++) {
-            maximum *= *i;
-        }
-        for (auto i = v2.begin(); i < v2.end(); i++)
-            minimum += *i;
-
-        std::cout << "The maximum and minimum are " << maximum;
-        std::cout << " and " << minimum << '.' << '\n';
+        std::cout << "The maximum and minimum are " << max << " and " << min << "." << '\n';
     }
-
-    return 0;
 }
