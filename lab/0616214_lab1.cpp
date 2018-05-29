e885fa5a6846a8596f93660040007c9a1329dbc0
finch185277 <59487andy@gmail.com>
2018-05-29 16:58:51 +0800

    Update 0616214_lab1.cpp

--- lab/0616214_lab1.cpp
+++ lab/0616214_lab1.cpp
@@ -1,33 +1,32 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-void count_digit(string num, int SIZE)
+void count_digit(std::string num, int SIZE)
 {
-        if(SIZE > 1)
-        {
-                int sum = 0;
-                for(int i = 0; i < SIZE; i++)
-                {
-                        sum = sum + num[i] - '0'; //string to int
-                }
-                //string ts = to_string(sum); //QQ not work
-                string ts;
-                stringstream tem; //Why~
-                tem << sum;
-                tem >> ts;
-                count_digit(ts, ts.size());
+    if (SIZE > 1) {
+        int sum = 0;
+        for (int i = 0; i < SIZE; i++) {
+            sum = sum + num[i] - '0'; //string to int
         }
-        else
-        {
-                cout << num[0] << "\n";
-        }
+        std::string ts;
+        std::stringstream tem;
+        tem << sum;
+        tem >> ts;
+        count_digit(ts, ts.size());
+    } else {
+        std::cout << num[0] << "\n";
+    }
 }
 int main()
 {
-        string num;
-        while(cin >> num && num != "0")
-        {
-                count_digit(num, num.size());
-        }
-        return 0;
+    std::string num;
+    while (std::cin >> num && num != "0") {
+        count_digit(num, num.size());
+    }
+    return 0;
 }
